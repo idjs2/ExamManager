86af007f6f0d3b11383b36adc6cd771dd4b1c625
Jeongsik <idjs1102@naver.com>
2025-04-04 10:55:40 +0900

    first-commit

---
+++ test/src/test/test1.java
@@ -0,0 +1,17 @@
+package test;
+
+public class test1 {
+	public static void main(String[] args) {
+		int i = 1;
+		float f = 1.1f;
+		double d = 1.2;
+		boolean b = true;
+		char c = 'a';
+		
+		System.out.println(i);
+		System.out.println(f);
+		System.out.println(d);
+		System.out.println(b);
+		System.out.println(c);
+	}
+}
