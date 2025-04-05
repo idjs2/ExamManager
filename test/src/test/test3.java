75ed11f1e82198e3e78f4ec4d0ce518bbe2c6e23
ksk611 <Ksk611>
2025-04-05 09:44:06 +0900

    cspi

--- test/src/test/test3.java
+++ test/src/test/test3.java
@@ -2,6 +2,10 @@ package test;
 
 public class test3 {
 public static void main(String[] args) {
-	System.out.println("안녕하세요");
+	
+	//충돌 확인중...
+	for (int i = 0; i < args.length; i++) {
+		System.out.println("안녕하세요");
+	}
 }
 }
